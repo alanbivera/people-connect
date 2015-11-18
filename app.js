e1f3f9ea1c1a9f1dba207fa3a91482ef79661bf6
milashkm <milash.mohanan@gmail.com>
2015-11-18 19:10:51 +0530

    Created server.

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var login = require('./routes/login');
+
 
 var app = express();
 
@@ -22,8 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/peopleconnect/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -58,3 +57,7 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/people-connect');
